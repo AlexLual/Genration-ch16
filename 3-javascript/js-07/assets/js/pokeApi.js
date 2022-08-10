aa935b4ed7386baaffe75fa7be6504daddde0295
Alex Hernandez Duran <hernandezduranluisalejandro4@gmail.com>
2022-08-10 08:39:14 -0500

    se agrega pokeApi

--- 3-javascript/js-07/assets/js/pokeApi.js
+++ 3-javascript/js-07/assets/js/pokeApi.js
@@ -1,35 +1,41 @@
-// URL a consumir
-urlPokemon = "https://pokeapi.co/api/v2/pokemon/pikachu"
+// URL a consumir:
 
+const urlPokemon = "https://pokeapi.co/api/v2/pokemon/3";
 
-//Elementos del Dom
-const imgPokemon = document.getElementById("img-pokemon")
-console.log(imgPokemon)
+// Elementos del DOM:
+
+const imgPokemon = document.getElementById("img-pokemon");
+console.log(imgPokemon);
+
 const idPokemon = document.getElementById("id-pokemon");
 console.log(idPokemon);
 
-const nombrePokemon = document.getElementById(nombre-pokemon)
-  console.log(nombrePokemon);
+const nombrePokemon = document.getElementById("nombre-pokemon");
+console.log(nombrePokemon);
 
+//Funciones:
+async function obtenerPokemon(url) {
+  const respuesta = await fetch(url);
+  const datos = await respuesta.json();
 
-//Funciones
-async function obetenerPokemon(url) {
-  const respuesta = await fetch(url)
-  const datos = await respuesta.json()
- /*  console.log(datos);
-  console.log(datos.forms[0].name);
-  console.log(datos.abilities);
-  console.log(datos.id);
-  console.log(datos.types);
-  console.log(datos.sprites.other["official-artwork"].front_default); */
+  /*     console.log(datos);
+    console.log(datos.forms[0].name);
+    console.log(datos.abilities);
+    console.log(datos.id);
+    console.log(datos.types);
+    console.log(datos.sprites.other["official-artwork"].front_default);
+ */
 
   const pokemon = {
     nombre: datos.forms[0].name,
     habilidad: datos.abilities,
-    numero: datos.id,
-    tipo: datos.types,
-    imagem: datos.sprites.other["official-artwork"].front_default
-  }
-  console.log(pokemon.imagen);
+    id: datos.id,
+    tipos: datos.types,
+    imagen: datos.sprites.other["official-artwork"].front_default,
+  };
+
+  imgPokemon.src = pokemon.imagen;
+  idPokemon.textContent = `ID: ${pokemon.id}`; //cONCATENAMOS EL VALOR JALADO CON EL ID:
+  nombrePokemon.textContent = pokemon.nombre;
 }
-obetenerPokemon(urlPokemon)
+obtenerPokemon(urlPokemon);
