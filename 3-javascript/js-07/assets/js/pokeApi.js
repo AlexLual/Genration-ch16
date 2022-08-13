f937c3aa3995b68d3ec23466d1b97c487777c46f
Alex Hernandez Duran <hernandezduranluisalejandro4@gmail.com>
2022-08-12 19:26:55 -0500

    Se agregaron carpeta js09 y se termino pokeapi

--- 3-javascript/js-07/assets/js/pokeApi.js
+++ 3-javascript/js-07/assets/js/pokeApi.js
@@ -1,31 +1,40 @@
 // URL a consumir:
 
-const urlPokemon = "https://pokeapi.co/api/v2/pokemon/3";
+const urlPokemon = "https://pokeapi.co/api/v2/pokemon/";
 
 // Elementos del DOM:
 
 const imgPokemon = document.getElementById("img-pokemon");
-console.log(imgPokemon);
 
 const idPokemon = document.getElementById("id-pokemon");
-console.log(idPokemon);
 
 const nombrePokemon = document.getElementById("nombre-pokemon");
-console.log(nombrePokemon);
+
+const listaHabilidades = document.getElementById("lista-habilidades");
+
+const listaTipos = document.getElementById("tipos")
+
+const formulario = document.getElementById("buscar-pokemon");
+
+//Evento
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const inputPokemon = document.getElementById("busqueda-pokemon");
+  console.log(inputPokemon.value);
+const nuevaBusqueda = urlPokemon + inputPokemon.value
+console.log(nuevaBusqueda);
+  
+  obtenerPokemon(nuevaBusqueda);
+
+
+})
 
 //Funciones:
 async function obtenerPokemon(url) {
+ try{
   const respuesta = await fetch(url);
   const datos = await respuesta.json();
-
-  /*     console.log(datos);
-    console.log(datos.forms[0].name);
-    console.log(datos.abilities);
-    console.log(datos.id);
-    console.log(datos.types);
-    console.log(datos.sprites.other["official-artwork"].front_default);
- */
-
   const pokemon = {
     nombre: datos.forms[0].name,
     habilidad: datos.abilities,
@@ -33,9 +42,37 @@ async function obtenerPokemon(url) {
     tipos: datos.types,
     imagen: datos.sprites.other["official-artwork"].front_default,
   };
-
+  //imagen
   imgPokemon.src = pokemon.imagen;
-  idPokemon.textContent = `ID: ${pokemon.id}`; //cONCATENAMOS EL VALOR JALADO CON EL ID:
+  idPokemon.textContent = `ID: ${pokemon.id}`; //CONCATENAMOS EL VALOR JALADO CON EL ID:
   nombrePokemon.textContent = pokemon.nombre;
+
+
+  //Habilidades
+  let template = ``
+
+  for (let i = 0; i < pokemon.habilidad.length; i++) {
+    const nombreHabilidad = pokemon.habilidad[i].ability.name;
+
+
+    template = `<li class="list-group-item"> ${nombreHabilidad} </li>`;
+    
+  }
+
+  listaHabilidades.innerHTML = template
+  //Tipos
+
+  let templeTipos = ""
+ 
+  pokemon.tipos.forEach((tipo) => {
+    const nombreTipo = tipo.type.name;
+    
+    templeTipos = `<li class="list-group-item">${nombreTipo}</li>`;
+  })
+  
+  listaTipos.innerHTML = templeTipos
+} catch (e) {
+  
+  alert("Pokemon no Descubierto... aun ")
 }
-obtenerPokemon(urlPokemon);
+}
